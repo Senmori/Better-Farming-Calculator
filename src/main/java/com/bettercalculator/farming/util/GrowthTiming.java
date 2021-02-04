a63e19a4281a2d82bad8da3ad808c1f3b906d1b0
Senmori <thesenmori@gmail.com>
2021-02-03 23:48:51 -0500

    Add GrowthTiming to have a central place to store growth tick lengths.

---
+++ src/main/java/com/bettercalculator/farming/util/GrowthTiming.java
@@ -0,0 +1,29 @@
+package com.bettercalculator.farming.util;
+
+import com.bettercalculator.farming.crop.GrowthCycle;
+import lombok.Getter;
+
+@Getter
+public enum GrowthTiming
+{
+	FIVE_MINUTES(5),
+	TEN_MINUTES(10),
+	_20_MINUTES(20),
+	_40_MINUTES(40),
+	_80_MINUTES(80),
+	_160_MINUTES(160),
+	_320_MINUTES(320),
+	_640_MINUTES(640),
+	;
+
+	private final int length;
+	GrowthTiming(int lengthInMinutes)
+	{
+		this.length = lengthInMinutes;
+	}
+
+	public GrowthCycle toGrowthCycle(int numberOfCycles)
+	{
+		return GrowthCycle.of(getLength(), numberOfCycles);
+	}
+}
